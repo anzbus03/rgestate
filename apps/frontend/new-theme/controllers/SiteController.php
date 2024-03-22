28a945f6efa1b1dfcb4a2ed3a69d524d269a6569
Ahmed Nasser <anzbus03@gmail.com>
2024-03-22 17:45:18 +0400

    fix(listing): fix lisiting sale and rent sub categories and location

--- apps/frontend/new-theme/controllers/SiteController.php
+++ apps/frontend/new-theme/controllers/SiteController.php
@@ -1645,11 +1645,11 @@ Jq4pd48R
             // $htm = $this->renderPartial('_list_sub_categories', compact('formData', 'adModel', 'subCategories'), true, false);
         // } 
         else if (isset($formData['state']) && isset($formData['type_of']) && isset($formData['sub_category'])) {
-            $htm = $this->renderPartial('_list_location', compact('formData', 'adModel'), true, false);
+			$htm = $this->renderPartial('_list_location', compact('formData', 'adModel'), true, false);
         } else if (!isset($formData['state']) && isset($formData['type_of']) && !isset($formData['sub_category'])) {
-            $htm = $this->renderPartial('_list_location_city', compact('formData', 'adModel'), true, false);
+			$htm = $this->renderPartial('_list_location_city', compact('formData', 'adModel'), true, false);
         } else if (isset($formData['state']) && !in_array(strtolower($formData['state']), array('fujairah', 'umm-al-quwain', 'ras-al-khaimah', 'al-ain', 'ajman', 'sharjah', 'abu-dhabi', 'dubai')) && !isset($formData['sub_category'])) {
-            $htm = $this->renderPartial('_list_location_city', compact('formData', 'adModel'), true, false);
+			$htm = $this->renderPartial('_list_location_city', compact('formData', 'adModel'), true, false);
         } else {
 			$htm = $this->renderPartial('_list_location', compact('formData', 'adModel'), true, false);
         }
@@ -1715,14 +1715,12 @@ Jq4pd48R
                 
                 $htm = $this->renderPartial('_list_sub_categories', compact('formData', 'adModel', 'subCategories'), true, false);		
             }else {
-    			$htm = $this->renderPartial('_list_location', compact('formData', 'adModel'), true, false);
+    			$htm = $this->renderPartial('_list_location_business', compact('formData', 'adModel'), true, false);
             }
         }else if (!isset($formData['nested_sub_category'])){
 			$htm = $this->renderPartial('_list_nested_sub_categories', compact('formData', 'adModel'), true, false);
         }else if (!isset($formData['state'])) {
-			$htm = $this->renderPartial('_list_location', compact('formData', 'adModel'), true, false);
-		}else if (isset($formData['state'])) {
-            $htm = $this->renderPartial('_list_location', compact('formData', 'adModel'), true, false);
+			$htm = $this->renderPartial('_list_location_business', compact('formData', 'adModel'), true, false);
 		}
 
 		//$htm = $this->renderPartial('_list_categories',compact('new_homes','formData'),true,false);
--- apps/frontend/new-theme/views/site/_list_location_business.php
+++ apps/frontend/new-theme/views/site/_list_location_business.php
@@ -1,27 +1,31 @@
 <?php
-	 $cityDats = States::model()->byIdIterate();
-//  print_r($formData);
+ $cityDats = States::model()->byIdIterate();
 $adModelCriteria =	$adModel->findAds($formData ,false,true); 
 $adModelCriteria->select= 't.state as city_name,count(t.id) as id   ';
 $adModelCriteria->join.= ' LEFT JOIN {{states}} city ON t.state = city.state_id  ';
+$adModelCriteria->join  .= ' left JOIN {{subcategory}} subcat ON subcat.sub_category_id = t.sub_category_id ';
+$adModelCriteria->join  .= ' left JOIN {{subcategory}} nestsubcat ON nestsubcat.sub_category_id = t.nested_sub_category ';
+$adModelCriteria->condition .= ' and nestsubcat.slug=:nested_sub_category and subcat.slug=:sub_category_id ';
+$adModelCriteria->params[':nested_sub_category'] = $formData['nested_sub_category'];
+$adModelCriteria->params[':sub_category_id'] = $formData['sub_category'];
 $adModelCriteria->group = 't.state'; 
 $adModelCriteria->order  ='city.state_name asc '; 
 $new_homes =  $adModel->findAll($adModelCriteria);
- unset($formData['section_id']); unset($formData['country']);unset($formData['reg_id']);
-  if(isset($formData['preleased'])){
-          unset($formData['preleased']);
-          $formData['sec'] = 'preleased'; 
-      }
-  $create_array = array();
+unset($formData['section_id']); unset($formData['country']);unset($formData['reg_id']);
+if(isset($formData['preleased'])){
+    unset($formData['preleased']);
+      $formData['sec'] = 'preleased'; 
+   }
+   $create_array = array();
  foreach($formData as $k1=>$v1){
-     if(!in_array($k1,array('sec','type_of','state','reg','category'))){
+     if(!in_array($k1,array('sec','type_of','state','reg','category','sub_category','nested_sub_category'))){
          unset($formData[$k1]);
          $create_array[$k1] = $v1; 
      }
  }
  $query = '?';
  if(!empty($create_array)){
-      
+
      $query .= http_build_query($create_array);
  }
  if($query=='?'){ $query =''; }
@@ -40,7 +44,7 @@ if(!empty($new_homes)){
          }
 		 $formData1['state'] = $slug;
 		 $dClass = ($count>='41') ? 'd-none hideles' : ''; 
-    	echo '<li class="'.$dClass.' col-sm-3"><p><a href="'.Yii::app()->createUrl('listing/index', $formData1 ).$query.'">'.$name.'<span> ('.$v->id.')</span></a></p></li>';
+    	echo '<li class="'.$dClass.' col-sm-3"><p><a href="'.Yii::app()->createUrl('business_listing/index', $formData1 ).$query.'">'.$name.'<span> ('.$v->id.')</span></a></p></li>';
     	$count++;
 	}
     echo '</ul>';
