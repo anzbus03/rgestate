6ebf120d243a9bd4ebaeb9e9c2f0bfd911d423a0
Ahmed Nasser <anzbus03@gmail.com>
2024-07-12 15:54:55 +0400

    fix(forms): fix crm forms

--- apps/frontend/new-theme/controllers/SiteController.php
+++ apps/frontend/new-theme/controllers/SiteController.php
@@ -2046,7 +2046,7 @@ Jq4pd48R
 			if(!$model->save()){
 				echo json_encode(array('status'=>'0','msg'=>'<div class="alert alert-danger1"><strong>Error!</strong> '.CHtml::errorSummary($model).'. </div>'));
 			}else{
-				echo json_encode(array('status'=>'1','name'=>$model->name , 'msg'=>'<div class="alert alert-success"><strong>Success!</strong> Succesfully submited. </div>'));
+				echo json_encode(array('status'=>'1','name'=>$model->email , 'msg'=>'<div class="alert alert-success"><strong>Success!</strong> Succesfully submited. </div>'));
 			}
 		}
 	
