1fc06437898448d0171d699be93966ad46749a61
Ahmed Nasser <anzbus03@gmail.com>
2024-10-21 17:55:02 +0400

    fix(businessListing)

--- apps/frontend/new-theme/controllers/Business_listingController.php
+++ apps/frontend/new-theme/controllers/Business_listingController.php
@@ -53,7 +53,11 @@ class Business_listingController extends Controller
                 if ($mainRegion) {
                     $_GET['state'] = $mainRegion->slug;
                     unset($_GET['type_of']);
-                }
+                }else {
+					$mainRegion = States::model()->findByAttributes(['slug' => $subCategory]);
+					$_GET['state'] = $mainRegion->slug;
+                    unset($_GET['type_of']);
+				}
             }
         }
         if (isset($_GET['sub_category'])) {
@@ -61,7 +65,7 @@ class Business_listingController extends Controller
             $subCategoryModel = Subcategory::model()->findByAttributes(['slug' => $subCategory]);
         
             if (!$subCategoryModel) {
-                $mainRegion = MainRegion::model()->findByAttributes(['slug' => $subCategory]);
+                $mainRegion = States::model()->findByAttributes(['slug' => $subCategory]);
         
                 if ($mainRegion) {
                     $_GET['state'] = $mainRegion->slug;
