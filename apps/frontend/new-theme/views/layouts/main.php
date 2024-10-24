c13b2f6409bb4579ef945f8e710d9ed2fa9b6ba3
Ahmed Nasser <anzbus03@gmail.com>
2024-10-24 10:31:04 +0400

    fix(blog): sharing and listing

--- apps/frontend/new-theme/views/layouts/main.php
+++ apps/frontend/new-theme/views/layouts/main.php
@@ -22,11 +22,10 @@ $en = Yii::t('app',CURRENT_URL,array('rgestate.com/ar/'=>'rgestate.com/'));
 <meta property="og:type" content="website" />
 <meta property="og:title" content="<?php  echo  $pageTitle ;  ?>" />
 <meta property="og:description" content="<?php echo $meta_description;?>" />
+<meta property="og:image" content="<?php echo $featuredImageUrl; ?>"/>
 <meta property="og:url" content="<?php echo CURRENT_URL;?>" />
 <meta property="og:site_name" content="<?php echo $this->project_name;?>"/>
 <meta property="og:email" content="sales@rgestate.com"/>
-<!-- <meta property="og:image" content="<?php echo $featuredImageUrl; ?>"/> -->
-<meta property="og:image" content="http://webdevelopmentscripts.com/post-images/685b-change-browser-address-bar-color-chrome-android.jpeg"/>
 <meta property="og:phone_number" content="+971552792403"/>
 <meta property="og:site_name" content="RGEstate.Com By Riveria Global Group" />
 <meta property="article:publisher" content="https://www.facebook.com/RGEstateUAE"/>
