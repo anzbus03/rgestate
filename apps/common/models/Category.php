0a1447a724da3c270d4633e12f949035270b4522
Ahmed Nasser <anzbus03@gmail.com>
2024-12-24 11:51:14 +0400

    fixCache

--- apps/common/models/Category.php
+++ apps/common/models/Category.php
@@ -843,15 +843,15 @@ public function beforeSave(){
 	}
 	
 	public function  ListDataForJSON_ID_BySEctionNewSlugCacheNew($section=null){
-		$cacheKey =  'category_cache1306'.Yii::app()->options->get('system.common.category_cache','12').$section;
-		if(defined('LANGUAGE')){$cacheKey .= LANGUAGE ;   }
-		if ($items = Yii::app()->cache->get($cacheKey) and !isset($_GET['refresh'])) {
+		// $cacheKey =  'category_cache1306'.Yii::app()->options->get('system.common.category_cache','12').$section;
+		// if(defined('LANGUAGE')){$cacheKey .= LANGUAGE ;   }
+		// if ($items = Yii::app()->cache->get($cacheKey) and !isset($_GET['refresh'])) {
 			
-			 return $items;
-		}  
+		// 	 return $items;
+		// }  
 		 
 		$items = $this->ListDataForJSON_ID_BySEctionNewSlugNew($section);
-		Yii::app()->cache->set($cacheKey, $items,60 * 60 * 24 * 360  );
+		// Yii::app()->cache->set($cacheKey, $items,60 * 60 * 24 * 360  );
 		return $items; 
 	}
 		public function  ListDataForJSON_ID_BySEctionNewSlugNew($section)
